93df3656ec9357fd89aed94558cae2c78f871088
Igor Clemente <igorelectrofest2011@hotmail.com>
2020-03-11 01:40:57 -0300

    BOOTCAMP_GOSTACK_MODULE11_READMEFILE_UPDATED_11032020

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,12 +1,12 @@
-import faker from "faker";
-import { factory } from "factory-girl";
+import faker from 'faker';
+import { factory } from 'factory-girl';
 
-import User from "../src/app/models/User";
+import User from '../src/app/models/User';
 
-factory.define("User", User, {
+factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
-  password: faker.internet.password()
+  password: faker.internet.password(),
 });
 
 export default factory;
--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,45 +1,45 @@
-import request from "supertest";
-import bcrypt from "bcryptjs";
+import request from 'supertest';
+import bcrypt from 'bcryptjs';
 
-import app from "../../src/app";
-import truncate from "../../src/util/truncate";
+import app from '../../src/app';
+import truncate from '../../src/util/truncate';
 
-import factory from "../factories";
+import factory from '../factories';
 
-describe("User", () => {
+describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
 
-  it("should encrypt user password when new user created", async () => {
-    const user = await factory.create("User", {
-      password: "1234567"
+  it('should encrypt user password when new user created', async () => {
+    const user = await factory.create('User', {
+      password: '1234567',
     });
 
-    const compareHash = await bcrypt.compare("1234567", user.password_hash);
+    const compareHash = await bcrypt.compare('1234567', user.password_hash);
 
     expect(compareHash).toBe(true);
   });
 
-  it("should be able to register", async () => {
-    const user = await factory.attrs("User");
+  it('should be able to register', async () => {
+    const user = await factory.attrs('User');
 
-    const response = await request("http://localhost:3333")
-      .post("/users")
+    const response = await request('http://localhost:3333')
+      .post('/users')
       .send(user);
 
-    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty('id');
   });
 
-  it("should be able to register with duplicated email", async () => {
-    const user = await factory.attrs("User");
+  it('should be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
 
-    await request("http://localhost:3333")
-      .post("/users")
+    await request('http://localhost:3333')
+      .post('/users')
       .send(user);
 
-    const response = await request("http://localhost:3333")
-      .post("/users")
+    const response = await request('http://localhost:3333')
+      .post('/users')
       .send(user);
 
     expect(response.status).toBe(400);
